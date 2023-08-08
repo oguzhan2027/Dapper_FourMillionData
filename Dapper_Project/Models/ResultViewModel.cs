16db37f429a0ea2cf51cbc10ee08f1d738fba63c
oguzhan celik <oguzhancelik2027@gmail.com>
2023-08-09 01:35:54 +0300

    Add project files.

---
+++ Dapper_Project/Models/ResultViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Dapper_Project.DAL.DTOs;
+
+namespace Dapper_Project.Models
+{
+    public class ResultViewModel
+    {
+        public List<BrandResult>? BrandMax { get; set; }
+        public List<BrandResult>? BrandMin { get; set; }
+        public List<PlateResult>? PlateMax { get; set; }
+        public List<PlateResult>? PlateMin { get; set; }
+        public List<ShiftTypeResult>? ShiftType { get; set; }
+        public List<FuelResult>? FuelType { get; set; }
+        public List<CaseTypeResult>? CaseType { get; set; }
+
+    }
+}
